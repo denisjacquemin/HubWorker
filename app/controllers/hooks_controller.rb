f903360a458418e44f67b56d4a51fbea40efa381
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-02-09 22:21:54 +0100

    adding savedata webhook

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -4,6 +4,7 @@ class HooksController < ApplicationController
     render text: 'pong', :status => 200
   end
 
+  # http://hubworker.herokuapp.com/hooks/validatexml?filename=properties_20150129190212.xml&agent_id=immo356&entity_type=properties
   # curl -i 'http://localhost:3000/hooks/handleproperties?filename=properties_20150129190212.xml&agent_id=immo356'
   def validate_xml
     
@@ -17,11 +18,11 @@ class HooksController < ApplicationController
   end
   
   def save_data
-    #client = IronWorkerNG::Client.new
-    #task_id = client.tasks.create('SaveData',
-    #                              {:agent_id    => params[:agent_id],
-    #                               :entity_type => 'properties',
-    #                               :file_name   => params[:filename]} )                               
+    client = IronWorkerNG::Client.new
+    task_id = client.tasks.create('SaveData',
+                                  {:agent_id    => params[:agent_id],
+                                   :entity_type => params[:entity_type],
+                                   :file_name   => params[:filename]} )                             
     render nothing: true, :status => 200
   end
 
--- workers/validate_xml_worker.rb
+++ workers/validate_xml_worker.rb
@@ -5,11 +5,18 @@ require 'net/http'
 
 # FTP_HOST configured in hud.iron.io dashboard
 # agent_id, entity_type and file_name from payload
-path_to_xml = config['FTP_HOST'] + params['agent_id'] + '/' + params['entity_type'] + '/' + params['file_name']
+
+ftp_host = config['FTP_HOST']
+hooks_base_uri = config['HOOKS_BASE_URI']
+agent_id = params['agent_id']
+entity_type = params['entity_type']
+file_name = params['file_name']
+
+path_to_xml =  ftp_host + agent_id + '/' + entity_type + '/' + file_name
 
 #path_to_xml = 'http://hubworker.herokuapp.com/immo356/properties/properties_20150129190212.xml'
 
-path_to_xsd = 'xsd/' + params['entity_type'] + '.xsd' # 
+path_to_xsd = 'xsd/' + params['entity_type'] + '.xsd'
 
 schema = Nokogiri::XML::Schema(File.read(path_to_xsd))
 document = Nokogiri::XML(open(path_to_xml)) # build
@@ -18,15 +25,19 @@ errors = schema.validate(document).collect { |error|
   error.to_s
 }
 
-if errors.empty? {
+if errors.empty?
   # no errors call webhook /savedata
-  uri = URI.parse(config['HOOKS_BASE_URI'] + 'savedata'),
-  params = { :agent_id => params['agent_id'], :entity_type => params['entity_type'], :file_name => params['file_name']}
+  uri = URI.parse( hooks_base_uri + 'savedata')
+  params = { 
+    :agent_id     => agent_id, 
+    :entity_type  => entity_type, 
+    :file_name    => file_name 
+  }
   uri.query = URI.encode_www_form( params )
   Net::HTTP.get(uri)
-}
+end
 
-unless errors.empty? {
+unless errors.empty?
   # save errors to 
-}
-  
+  puts 'something'
+end
